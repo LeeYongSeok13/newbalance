d8e5931cf6e1adcd7bfa728571b12a8c3a8e59f6
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-07-12 17:49:19 +0900

    20220712

--- js/main.js
+++ js/main.js
@@ -3,5 +3,23 @@ $(function () {
         $('#ad_banner').slideUp();
     });
 
-    $('.main_slider').slick();
+    $('.main_slider').slick({
+        dots: true,
+        arrows: false,
+    });
+
+    $('.shirt_slider').slick({
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false,
+    });
+
+    $('.xi-arrow-left').on('click', function () {
+        $('.shirt_slider').slick('slickPrev');
+    });
+    $('.xi-arrow-right').on('click', function () {
+        $('.shirt_slider').slick('slickNext');
+    });
 })
