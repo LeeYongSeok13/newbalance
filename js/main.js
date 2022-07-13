235ebb5ed8126909dcf12c5b7d0b00651ab75fc7
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-07-13 17:47:29 +0900

    20220713

--- js/main.js
+++ js/main.js
@@ -6,7 +6,17 @@ $(function () {
     $('.main_slider').slick({
         dots: true,
         arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
     });
+
+    $('#model .controls i:nth-child(1)').on('click', function () {
+        $('.iu').trigger('pause');
+    });
+    $('#model .controls i:nth-child(2)').on('click', function () {
+        $('.iu').trigger('play');
+    })
 
     $('.shirt_slider').slick({
         slidesToShow: 4,
@@ -21,5 +31,12 @@ $(function () {
     });
     $('.xi-arrow-right').on('click', function () {
         $('.shirt_slider').slick('slickNext');
+    });
+
+    $('.tab_menu figure').on('click', function (event) {
+        event.preventDefault();
+        var idx = $(this).index();
+        $('.tab_content>div').removeClass('on');
+        $('.tab_content>div').eq(idx).addClass('on');
     });
 })
